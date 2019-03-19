4974a4705f85f19b1b8a4794cb2c7ac028758a26
Delisa Mason <iskanamagus@gmail.com>
2019-03-19 15:20:37 +0000

    tests(maze): Run maze on mac using the MAZE_OS env var
    If set to "mac", runs the scenarios on the Mac. Otherwise runs on iOS.
    
    Bumps cocoapods version to work around issue where the build would fail
    due to a script error in a build phase.

--- features/steps/ios_steps.rb
+++ features/steps/ios_steps.rb
@@ -9,26 +9,38 @@ When("I run {string}") do |event_type|
 end
 
 When("I launch the app") do
-  wait_time = '4'
-  steps %Q{
-    When I run the script "features/scripts/launch_ios_app.sh"
-    And I wait for #{wait_time} seconds
-  }
+  if RUNNING_MAC
+    steps %Q{
+      When I run the script "features/scripts/launch_mac_app.sh"
+      And I wait for 2 seconds
+    }
+  else # Default to iOS
+    wait_time = '4'
+    steps %Q{
+      When I run the script "features/scripts/launch_ios_app.sh"
+      And I wait for #{wait_time} seconds
+    }
+  end
 end
 When("I relaunch the app") do
-  wait_time = '4'
-  steps %Q{
-    When I run the script "features/scripts/launch_ios_app.sh"
-    And I wait for #{wait_time} seconds
-  }
+  step("I launch the app")
 end
 When("I crash the app using {string}") do |event|
   steps %Q{
     When I set environment variable "EVENT_TYPE" to "#{event}"
+    And I set environment variable "BUGSNAG_API_KEY" to "a35a2a72bd230ac0aa0f52715bbdc6aa"
     And I set environment variable "EVENT_MODE" to "normal"
     And I launch the app
     And I set environment variable "EVENT_MODE" to "noevent"
   }
+end
+
+Then("the payload notifier name is correct") do
+  if RUNNING_MAC
+    step('the payload field "notifier.name" equals "OSX Bugsnag Notifier"')
+  else
+    step('the payload field "notifier.name" equals "iOS Bugsnag Notifier"')
+  end
 end
 
 Then("the payload field {string} of request {int} equals the payload field {string} of request {int}") do |field1, request_index1, field2, request_index2|
@@ -44,10 +56,7 @@ Then("the payload field {string} of request {int} does not equal the payload fie
 end
 
 When("I corrupt all reports on disk") do
-  app_path = `xcrun simctl get_app_container booted com.bugsnag.iOSTestApp`.chomp
-  app_path.gsub!(/(.*Containers).*/, '\1')
-  files = Dir.glob("#{app_path}/**/KSCrashReports/iOSTestApp/*.json")
-  files.each do |path|
+  Dir.glob(report_file_glob).each do |path|
     File.open(path, 'w') {|file| file.truncate(0) }
   end
 end
@@ -83,4 +92,34 @@ Then("the event {string} is within {int} seconds of the current timestamp") do |
   thenSecs = Time.parse(value).to_i
   delta = nowSecs - thenSecs
   assert_true(delta.abs < threshold_secs, "Expected current timestamp, but received #{value}")
+end
+
+Then("the payload field {string} equals the running OS name") do |field|
+  if RUNNING_MAC
+    step("the payload field \"#{field}\" equals \"macOS\"")
+  else
+    step("the payload field \"#{field}\" equals \"iOS\"")
+  end
+end
+
+Then("the payload field {string} equals the print-formatted OS name") do |field|
+  if RUNNING_MAC
+    step("the payload field \"#{field}\" equals \"Mac OS\"")
+  else
+    step("the payload field \"#{field}\" equals \"iOS\"")
+  end
+end
+
+Then("the payload field {string} equals the running OS version") do |field|
+  if RUNNING_MAC
+    step("the payload field \"#{field}\" equals \"#{`sw_vers -productVersion`.chomp}\"")
+  else
+    step("the payload field \"#{field}\" equals \"11.2\"")
+  end
+end
+
+Then("the payload field {string} equals the current device model") do |field|
+  unless RUNNING_MAC # we don't store model on mac
+    step("the payload field \"#{field}\" equals \"iPhone10,4\"")
+  end
 end
